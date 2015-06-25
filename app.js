3e74a577f6334b255fdf0a26c5146be133619662
Thibaut Van Spaandonck <thibaut@urge2code.com>
2015-06-25 15:35:46 +0200

    Port 3000 not hardcoded anymore, used as fallback (solve "bad gateway" on AWS)

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
 var express = require('express');
 var routes = require('./routes');
 var app = express();
+var port = process.env.PORT || 3000;
 routes(app);
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
   var host = server.address();
   console.log('API running at http://%s:%s', host.address, host.port);
 });
