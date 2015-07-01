df3c93fee9065dc8b8670ce8f11b4e8c378470c6
Thibaut Van Spaandonck <thibaut@urge2code.com>
2015-07-01 15:41:53 +0200

    Configured CORS

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var express = require('express');
+var cors = require('cors');
 var routes = require('./routes');
 var app = express();
-var port = process.env.PORT || 3000;
+
+app.use(cors());
 routes(app);
 
-var server = app.listen(port, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address();
   console.log('API running at http://%s:%s', host.address, host.port);
 });
