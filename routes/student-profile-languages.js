a4b7586fff16db3994cce0e291de896cd229954a
Thibaut Van Spaandonck <thibaut@urge2code.com>
2015-11-22 13:59:24 +0100

    [Feature] Student profile : languages edit feature (WIP)

--- routes/student-profile-languages.js
+++ routes/student-profile-languages.js
@@ -6,4 +6,17 @@ module.exports = (app, models, sequelizeUtils, HttpStatus) => {
   app.get('/studentProfileLanguages/:id', (req, res, next) => {
     sequelizeUtils.findById(StudentProfileLanguage, req, res, next);
   });
+
+  app.put('/studentProfileLanguages/:id', (req, res, next) => {
+    let language = req.body.studentProfile.studentProfileLanguage;
+    let values = {
+      isMotherTongue: language.isMotherTongue,
+      level: language.level
+    };
+    sequelizeUtils.update(StudentProfileLanguage, values, req, res, next);
+  });
+
+  app.delete('/studentProfileLanguages/:id', (req, res, next) => {
+    sequelizeUtils.delete(StudentProfileLanguage, req, res, next);
+  });
 };
