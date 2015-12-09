dbf5d08c1ff6f61e0f7391d2ec24e0b1b130a0c0
Thibaut Van Spaandonck <thibaut@urge2code.com>
2015-12-09 21:34:04 +0100

    [Feature] Configured HTTPS

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 'use strict';
 
 const express = require('express');
-const bodyParser = require('body-parser');
+const https = require('https');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 const routes = require('./routes');
 const models = require('./models');
-const app = express();
 
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 routes.initialize(app, models);
 
-const server = app.listen(process.env.PORT || 3000, () => {
-  const host = server.address();
-  console.log('API running at http://%s:%s', host.address, host.port);
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'ssl', 'api_assent_io.key'), 'utf8'),
+  cert: fs.readFileSync(path.join(__dirname, 'ssl', 'api_assent_io.crt'), 'utf8')
+};
+const server = https.createServer(options, app).listen(443, null, null, () => {
+  console.log('API listening on port %d in %s mode', server.address().port, app.settings.env);
 });
