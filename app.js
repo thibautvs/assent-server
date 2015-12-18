d249194ccfe92142203da6d50980180556808834
Thibaut Van Spaandonck <thibaut@urge2code.com>
2015-12-18 12:09:30 +0100

    [Feature] Implemented logic for self-signed certificates (development mode)

--- app.js
+++ app.js
@@ -15,15 +15,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 routes.initialize(app, models);
 
-const options = {
-  key: fs.readFileSync(path.join(__dirname, 'ssl', 'api_assent_io.key'), 'utf8'),
-  cert: fs.readFileSync(path.join(__dirname, 'ssl', 'api_assent_io.crt'), 'utf8'),
-  ca: [
-    fs.readFileSync(path.join(__dirname, 'ssl', 'ca_bundle_part_01.crt'), 'utf8'),
-    fs.readFileSync(path.join(__dirname, 'ssl', 'ca_bundle_part_02.crt'), 'utf8'),
-    fs.readFileSync(path.join(__dirname, 'ssl', 'ca_bundle_part_03.crt'), 'utf8')
-  ]
-};
+let options;
+if (app.settings.env === 'development') {
+  options = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl', 'dev', 'localhost.key'), 'utf8'),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'dev', 'localhost.crt'), 'utf8'),
+    ca: [ fs.readFileSync(path.join(__dirname, 'ssl', 'dev', 'ca.crt'), 'utf8') ]
+  };
+} else {
+  options = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl', 'api_assent_io.key'), 'utf8'),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'api_assent_io.crt'), 'utf8'),
+    ca: [
+      fs.readFileSync(path.join(__dirname, 'ssl', 'ca_bundle_part_01.crt'), 'utf8'),
+      fs.readFileSync(path.join(__dirname, 'ssl', 'ca_bundle_part_02.crt'), 'utf8'),
+      fs.readFileSync(path.join(__dirname, 'ssl', 'ca_bundle_part_03.crt'), 'utf8')
+    ]
+  };
+}
+
 const server = https.createServer(options, app).listen(443, null, null, () => {
   /* 443 is the HTTPS port so that it offers the convenience to omit the port in the URL
    * (api.assent.io instead of api.assent.io:3000 for example). However, ports below 1024
