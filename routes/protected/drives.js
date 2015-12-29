3a7263e5c1e5c7c1c724e82a1e0273c318ef6d08
Thibaut Van Spaandonck <thibaut@urge2code.com>
2015-12-29 10:13:30 +0100

    [Feature] Implemented routes for "drives"

--- routes/protected/drives.js
+++ routes/protected/drives.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const validator = require('../../utils/validator');
+
 module.exports = (app, models, sequelizeUtils, HttpStatus) => {
   const Drive = models.Drive;
 
@@ -7,7 +9,45 @@ module.exports = (app, models, sequelizeUtils, HttpStatus) => {
     sequelizeUtils.findById(Drive, req, res, next);
   });
 
+  app.post('/drives', (req, res, next) => {
+    let drive = req.body.drive;
+    let isValid = validate(drive);
+    if (isValid) {
+      let values = {
+        factor: drive.factor,
+        description: drive.description,
+        position: drive.position,
+        student_profile_id: drive.studentProfile
+      };
+      sequelizeUtils.create(Drive, values, res, next);
+    } else {
+      res.status(HttpStatus.BAD_REQUEST).send({error: 'validation_failed'});
+    }
+  });
+
+  app.put('/drives/:id', (req, res, next) => {
+    let drive = req.body.drive;
+    let isValid = validate(drive);
+    if (isValid) {
+      let values = {
+        factor: drive.factor,
+        description: drive.description,
+        position: drive.position,
+      };
+      sequelizeUtils.update(Drive, values, req, res, next);
+    } else {
+      res.status(HttpStatus.BAD_REQUEST).send({error: 'validation_failed'});
+    }
+  });
+
   app.delete('/drives/:id', (req, res, next) => {
     sequelizeUtils.delete(Drive, req, res, next);
   });
 };
+
+function validate(drive) {
+  return validator.isValid([
+    validator.required(drive.factor),
+    validator.required(drive.description)
+  ]);
+}
